6831bcddb11825392cf4d330cc49664ca93204df
Ahilan <ahilan0809@gmail.com>
2025-12-11 16:45:38 -0800

    Initial Commit

---
+++ GlorifiedClock/GlorifiedClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlorifiedClockApp.swift
+//  GlorifiedClock
+//
+//  Created by Ahilan Kumaresan on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GlorifiedClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlorifiedClockTests/GlorifiedClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlorifiedClockTests.swift
+//  GlorifiedClockTests
+//
+//  Created by Ahilan Kumaresan on 11/12/2025.
+//
+
+import Testing
+@testable import GlorifiedClock
+
+struct GlorifiedClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
